502bd2f404c2fb79240160d680f7e3c72659c985
Nurul Huda <ibnujakaria@outlook.com>
2021-01-19 08:38:12 +0700

    persiapan belajar layout flutter (android)

---
+++ persiapan/android/app/src/main/kotlin/com/example/persiapan/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.persiapan
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ persiapan/lib/main.dart
@@ -0,0 +1,33 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(MaterialApp(
+    home: Scaffold(
+      body: SafeArea(
+        // child: Row(
+        // child: Column(
+        child: Wrap(
+          children: [
+            buatKotak(Colors.greenAccent, 100),
+            buatKotak(Colors.orangeAccent[400], 70),
+            buatKotak(Colors.greenAccent, 50),
+            buatKotak(Colors.orangeAccent[400], 90),
+            buatKotak(Colors.red[300], 110),
+            buatKotak(Colors.blue[300], 30),
+          ],
+        ),
+      )
+    ),
+  ));
+}
+
+Widget buatKotak (Color warna, double ukuran) {
+  return Container(
+    decoration: BoxDecoration(
+      color: warna
+    ),
+    height: ukuran,
+    width: ukuran,
+    margin: EdgeInsets.all(10),
+  );
+}
